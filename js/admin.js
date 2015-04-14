5c37b55705d38d41afe2a086aed7712ef2e99e4b
tbrathj17v3 <tbrathj17v3@gmail.com>
2015-04-14 19:30:07 -0400

    8

---
+++ js/admin.js
@@ -0,0 +1,9 @@
+$(function() {
+ 
+    Parse.$ = jQuery;
+ 
+    // Replace this line with the one on your Quickstart Guide Page
+    Parse.initialize("RLIF6gHptIpn0yRbB53E4G7LQa3x2vmfAMjlNW98", "tGu47uqtsH1DVBzfX9YUOOZSGM6DbPLvaIQrQwex");
+    
+ 
+});
