a0d50d1d0d1210dcf3a3cca383bbc48a5c3231d2
tbrathj17v3 <tbrathj17v3@gmail.com>
2015-04-14 18:39:19 -0400

    7

--- js/blog.js
+++ js/blog.js
@@ -1,14 +1,22 @@
-var Blog = Parse.Object.extend("Blog");
-var Blogs = Parse.Collection.extend({model: Blog});
+$(function() {
+ 
+    Parse.$ = jQuery;
+ 
+    // Replace this line with the one on your Quickstart Guide Page
+    Parse.initialize("RLIF6gHptIpn0yRbB53E4G7LQa3x2vmfAMjlNW98", "tGu47uqtsH1DVBzfX9YUOOZSGM6DbPLvaIQrQwex");
+    var Blog = Parse.Object.extend("Blog");
+	var Blogs = Parse.Collection.extend({model: Blog});
 
-// new instance of Blogs collection
-var blogs = new Blogs();
+	// new instance of Blogs collection
+	var blogs = new Blogs();
 
-blogs.fetch({
-    success: function(blogs) {
-        console.log(blogs);
-    },
-    error: function(blogs, error) {
-        console.log(error);
-    }
+	blogs.fetch({
+	    success: function(blogs) {
+	        console.log(blogs);
+	    },
+	    error: function(blogs, error) {
+	        console.log(error);
+	    }
+	});
+ 
 });
