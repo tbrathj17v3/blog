88b5141f4ce9e97b6b584ef50adf29bb9b8c293b
tbrathj17v3 <tbrathj17v3@gmail.com>
2015-04-14 18:24:54 -0400

    6

--- js/blog.js
+++ js/blog.js
@@ -1,13 +1,14 @@
-$(function() {
- 
-    Parse.$ = jQuery;
- 
-    // Replace this line with the one on your Quickstart Guide Page
-    Parse.initialize("RLIF6gHptIpn0yRbB53E4G7LQa3x2vmfAMjlNW98", "tGu47uqtsH1DVBzfX9YUOOZSGM6DbPLvaIQrQwex");
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({foo: "bar"}).then(function(object) {
-      alert("yay! it worked");
-    });
- 
+var Blog = Parse.Object.extend("Blog");
+var Blogs = Parse.Collection.extend({model: Blog});
+
+// new instance of Blogs collection
+var blogs = new Blogs();
+
+blogs.fetch({
+    success: function(blogs) {
+        console.log(blogs);
+    },
+    error: function(blogs, error) {
+        console.log(error);
+    }
 });
